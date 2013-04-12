edeb5d8c44469893b6a1a695b6001a27f632cf68
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-04-12 14:08:09 +0900

    Simplefied the project.

--- AssetBundleDependency/Assets/Runtime/PrefabLoader.js
+++ AssetBundleDependency/Assets/Runtime/PrefabLoader.js
@@ -9,10 +9,10 @@ function Start() {
 	yield WaitForSeconds(0.5);
 
 	var bundle1 = AssetBundle.CreateFromFile(path1);
-	Instantiate(bundle1.mainAsset, Vector3(0, 1, 0), Quaternion.identity);
+	Instantiate(bundle1.mainAsset, Vector3(0, -0.5, 0), Quaternion.identity);
 
 	yield WaitForSeconds(0.5);
 
 	var bundle2 = AssetBundle.CreateFromFile(path2);
-	Instantiate(bundle2.mainAsset, Vector3(0, 2, 0), Quaternion.identity);
+	Instantiate(bundle2.mainAsset, Vector3(0, 0.5, 0), Quaternion.identity);
 }
